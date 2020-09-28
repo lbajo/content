ae353b611f56dfc85dc9b7c7bcb98b828a3540c8
Lorena Bajo Rebollo <lbajo9@gmail.com>
2020-09-28 11:51:38 +0200

    New rules added for ROS2 syste

---
+++ linux_os/guide/ros2system/package_ros2_eloquent_installed/tests/installed.pass.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# profiles = xccdf_org.ssgproject.content_profile_common
+
+apt install ros-eloquent-desktop
---
+++ linux_os/guide/ros2system/package_ros2_eloquent_installed/tests/notinstalled.fail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# profiles = xccdf_org.ssgproject.content_profile_common
+
+sudo apt-get remove ros-eloquent-desktop
